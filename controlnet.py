81deba71dd50c358144f22da481357a268065fb4
El D00derino <you@example.com>
2026-06-23 21:26:53 +0000

    Refactor: Add mask support, enhance ControlNet logic, and transition to `ComfyNodeABC` inheritance

--- controlnet.py
+++ controlnet.py
@@ -3,6 +3,7 @@ from comfy.comfy_types.node_typing import IO, ComfyNodeABC
 from comfy.controlnet import load_controlnet as cn_load
 from .defs import COSY_CATEGORY, COSY_HASH, CONDPipe_t, _mk_hash_key, _hash_tensor
 from nodes import ControlNetApplyAdvanced as ContN
+import comfy.utils
 
 def load_controlnet(control_net_name):
     controlnet_path = folder_paths.get_full_path_or_raise("controlnet", control_net_name)
@@ -49,7 +50,7 @@ class ControlNet_enabled(ComfyNodeABC):
         _, _, _, enabled = controlnet_cf
         return enabled,
 
-class MaybeApplyControlNet(ContN):
+class MaybeApplyControlNet(ComfyNodeABC):
     @classmethod
     def __init__(self):
         self._cache = {}
@@ -64,46 +65,59 @@ class MaybeApplyControlNet(ContN):
                 "controlnet_cf": ("ControlNetCf",{}),
             },
             "optional": {
+                "mask": (IO.MASK,{"tooltip": "Optional mask, stretched to image size, applied to image before ControlNet processing. Black=IN"}),
                 "vae": (IO.VAE, {"lazy": True}),
             }
 
         }
-    RETURN_TYPES = (CONDPipe_t,)
-    RETURN_NAMES = ("CONDPipe",)
+    RETURN_TYPES = (CONDPipe_t, IO.IMAGE,)
+    RETURN_NAMES = ("CONDPipe", "image",)
     FUNCTION = "run"
     CATEGORY = COSY_CATEGORY
 
-    def check_lazy_status(self, CONDPipe, control_net, image, controlnet_cf, vae=None):
+    def check_lazy_status(self, CONDPipe, control_net, image, controlnet_cf, mask=None, vae=None):
         if controlnet_cf is None: return []  # Safety
         _, _, _, enabled = controlnet_cf
 
         if enabled:
             needed = ["image"]
+            if mask is not None: needed.append("mask")
             if vae is not None: needed.append("vae")
             return needed
         else:
             return []  # Don't load anything when disabled
 
-    def run(self, CONDPipe, control_net, image, controlnet_cf, vae=None):
+    def run(self, CONDPipe, control_net, image, controlnet_cf, mask = None, vae=None):
         positive, negative = CONDPipe
         strength, s_pct, e_pct, enabled = controlnet_cf
 
         if enabled:
+            print(f"Applying controlnet {control_net} to image {image.shape}")
             if self._cache.get("name") != control_net:
                 self._cache = {} #clear old one first so exception will not leave it half-baked.
                 self._cache = {
                     "name": control_net,
                     "model": load_controlnet(control_net),
                 }
-            positive, negative = ContN.apply_controlnet(self, positive, negative, self._cache["model"], image, strength, s_pct, e_pct, vae=vae)
-            cnet_hash = _mk_hash_key(self._cache["name"], _hash_tensor(image), strength, s_pct, e_pct)
+
+            extra_concat = []
+            if mask is not None:
+                print(f"Applying mask {mask.shape} to image {image.shape}")
+                m = 1.0 - mask.reshape((-1, 1, mask.shape[-2], mask.shape[-1]))
+                m_apply = comfy.utils.common_upscale(m, image.shape[2], image.shape[1], "bilinear", "disabled").round()
+                image = image * m_apply.movedim(1, -1).repeat(1, 1, 1, image.shape[3])
+                # print(f"Mask applied {m_apply.shape}, image now {image.shape}")
+                if getattr(self._cache["model"], "concat_mask", False): extra_concat = [m]
+
+            positive, negative = ContN().apply_controlnet(positive, negative, self._cache["model"], image, strength, s_pct, e_pct, vae=vae, extra_concat=extra_concat)
+            cnet_hash = _mk_hash_key(self._cache["name"], _hash_tensor(image), _hash_tensor(mask), strength, s_pct, e_pct)
             positive = self._add_control_hash(positive, cnet_hash)
             negative = self._add_control_hash(negative, cnet_hash)
 
         else:
             self._cache = {}
 
-        return (positive, negative),
+        return (positive, negative), image,
 
     def _add_control_hash(self, conditioning, cnet_hash):
         out = []
